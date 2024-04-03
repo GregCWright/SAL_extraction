d7552566048a5f72ceef36c1241045d3e3e2263d
Greg Wright <eggzo@Gregs-MBP.lan>
2024-04-03 21:44:27 +0100

    Correcting input terms

--- rust-producers/csv_insertion_handler/src/main.rs
+++ rust-producers/csv_insertion_handler/src/main.rs
@@ -16,7 +16,7 @@ struct Args {
     username: String,
 
     #[arg(short, long)]
-    password: String,
+    db_password: String,
 
     #[arg(short, long)]
     host: String,
@@ -38,7 +38,7 @@ async fn main()  -> Result<(), sqlx::Error> {
     let args = Args::parse();
 
     let postgres_username: String = args.username;
-    let postgres_password: String = args.password;
+    let postgres_password: String = args.db_password;
     let postgres_host: String = args.host;
     let postgres_port: String = args.port;
     let postgres_database: String = args.database;
